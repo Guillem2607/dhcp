78f67632e94d7b89cdca70b77301739013520b35
Guillem Lago <121028740+Guillem2607@users.noreply.github.com>
2024-03-26 12:55:29 +0100

    Update baja_MAC.sh

--- baja_MAC.sh
+++ baja_MAC.sh
@@ -8,14 +8,17 @@ mac_address="$2"
 # Función para eliminar del inventario
 
 delete_from_inventory() {
-    sed -i "/^host $pcname {$/,/^}$/d" /etc/dhcp/dhcpd.conf
+    # Utiliza grep para buscar el host en el archivo dhcpd.conf
+    if grep -q "host $pcname" /etc/dhcp/dhcpd.conf; then
+        sed -i "/^host $pcname {$/,/^}$/d" /etc/dhcp/dhcpd.conf
+    fi
 }
 
 # Cuerpo del script
 
-if grep -q "$pcname" /etc/dhcp/dhcpd.conf; then
+if grep -q "$mac_address" /etc/dhcp/dhcpd.conf && grep -q "$pcname" /etc/dhcp/dhcpd.conf; then
     delete_from_inventory
-    logger "[OK] MAC Address hac been deleted from the inventory"
+    logger "[OK] Host $pcname has been successfully removed."
 else
-
+    logger "[FAIL] Host $pcname does not exist."
 fi
